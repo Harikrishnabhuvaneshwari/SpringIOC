414df273d2b7db2e8bb335e7ea4dc171a8807a43
Harikrishnabhuvaneshwari <harikrishnabhuvaneshwari@yahoo.com>
2019-05-26 22:00:48 +0530

    #Setter and Constructor injection concepts.

---
+++ src/main/java/com/ioc/SpringIOC/setterinjection/Employee.java
@@ -0,0 +1,35 @@
+package com.ioc.SpringIOC.setterinjection;
+
+
+public class Employee {
+	private int employeeId;
+	private String employeeName;
+
+	private Employee(){
+		// eventhough the constructor is private still we can create bean for this class 
+		//using this constructor.
+	}
+
+	public Employee(int employeeId,String employeeName){
+		System.out.println("checking constructor injection");
+		this.employeeId=employeeId;
+		this.employeeName=employeeName;
+	}
+
+
+	public void setEmployeeId(int employeeId) {
+		System.out.println("Setter injection check");
+		this.employeeId = employeeId;
+	}
+
+
+	public void setEmployeeName(String employeeName) {
+		System.out.println("Setter injection check");
+		this.employeeName = employeeName;
+	}
+
+
+	public void employeeDetails(){
+		System.out.println("Employee Id : "+employeeId+" Employee Name : "+employeeName);
+	}
+}
---
+++ src/main/java/com/ioc/SpringIOC/setterinjection/SetterInjectionDriver.java
@@ -0,0 +1,16 @@
+package com.ioc.SpringIOC.setterinjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterInjectionDriver {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("beans.xml");
+		Employee employee=applicationContext.getBean("employee",Employee.class);
+		employee.employeeDetails();
+		Employee oldEmployee=applicationContext.getBean("oldEmployee",Employee.class);
+		oldEmployee.employeeDetails();
+	}
+
+}
